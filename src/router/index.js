5be57eed236faaeccf0400e1934cd56268cacde5
畅垒 <changlei-886@163.com>
2025-12-18 23:20:56 +0800

    一些文件删除

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Home from '../views/Home.vue'
 import Recharge from '../views/Recharge.vue'
 import Withdraw from '../views/Withdraw.vue'
-import Me from '../views/Me.vue'
 import Settings from '../views/Settings.vue'
 import Bill from '../views/Bill.vue'
 import WithdrawPassword from '../views/WithdrawPassword.vue'
@@ -12,7 +11,6 @@ const routes = [
   { path: '/recharge', name: 'Recharge', component: Recharge },
   { path: '/bill', name: 'Bill', component: Bill },
   { path: '/withdraw', name: 'Withdraw', component: Withdraw },
-  { path: '/me', name: 'Me', component: Me },
   { path: '/settings', name: 'Settings', component: Settings },
   { path: '/withdraw-password', name: 'WithdrawPassword', component: WithdrawPassword },
 ]
